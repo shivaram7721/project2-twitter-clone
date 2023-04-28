deb6dfe28440626d2848f1f43c437840cd004fdf
shubham-shinde1 <subhamshinde308@gmail.com>
2023-04-29 00:18:28 +0530

    side-bar-done

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,30 +5,24 @@ import { Footer } from "../../components/footer/footer";
 import { TweetArea } from "../../components/tweetArea/tweetArea";
 import { Grid } from "@mui/material";
 
-
-import homepage from "./Home.module.css"
-
+import homepage from "./Home.module.css";
 
 import FirstRightCard from "../RightSidePart/FirstRightCard";
 import SearchBar from "../RightSidePart/SearchBar";
 import SecondRightCard from "../RightSidePart/SecondRightCard";
-
-
+import Sidebar from "../Sidebar/Sidebar";
 
 export function Home() {
-
-
   return (
     <div>
-      <Grid container >
+      <Grid container>
         <Grid item md={2.5} xs={1}>
-
+          <Sidebar />
         </Grid>
 
-        <Grid item md={6} xs={11} >
+        <Grid item md={6} xs={11}>
           <TweetArea />
           <Tweets />
-
         </Grid>
 
         <Grid item md={3.5} className={homepage.rightPart}>
