73cdf309741b4d91b61d19c6c240f3e933f11590
shivaram7721 <shivaramdusa7721@gmail.com>
2023-04-28 17:18:31 +0530

    json removed

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,28 @@
 // import { getUsers } from "../../utils/localStorage";
 import { Tweets } from "../../components/tweets/tweets";
 import { Footer } from "../../components/footer/footer";
+import { TweetArea } from "../../components/tweetArea/tweetArea";
+import { Grid } from "@mui/material";
 
 export function Home() {
 
         
     return (
         <div>
-            {/* <h1>Home page</h1> */}
-            <Tweets />
+            <Grid container>
+                <Grid md={3} xs={1}>
+
+                </Grid>
+
+                <Grid md={6} xs={11}>
+                    <TweetArea />
+                    <Tweets />
+                </Grid>
+
+                <Grid md={3}>
+
+                </Grid>
+            </Grid>
             <Footer />
         </div>
     );
