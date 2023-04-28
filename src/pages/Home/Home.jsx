7cce978b0141eceae7d53fcb7543c717a3642eed
shivaram7721 <shivaramdusa7721@gmail.com>
2023-04-28 10:17:46 +0530

    tweets section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,15 @@
 // import { getUsers } from "../../utils/localStorage";
+import { Tweets } from "../../components/tweets/tweets";
+import { Footer } from "../../components/footer/footer";
 
 export function Home() {
 
         
-
     return (
         <div>
-            <h1>Home page</h1>
+            {/* <h1>Home page</h1> */}
+            <Tweets />
+            <Footer />
         </div>
     );
 }
